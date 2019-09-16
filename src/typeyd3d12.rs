56c986d58d820848cf127a75cb399733abfe8ffc
Fil Krynicki <filipkrynicki@gmail.com>
2019-09-15 17:32:42 -0700

    Untested wrapper for D3DCompileFromFile
    + also added flag type for compile flag params to that function

--- src/typeyd3d12.rs
+++ src/typeyd3d12.rs
@@ -15,7 +15,7 @@ use winapi::shared::minwindef::*;
 use winapi::shared::{dxgiformat, dxgitype, winerror};
 use winapi::um::d3d12::*;
 use winapi::um::d3d12sdklayers::*;
-use winapi::um::{d3dcommon, unknwnbase};
+use winapi::um::{d3dcommon, unknwnbase, d3dcompiler};
 use winapi::Interface;
 
 use wio::com::ComPtr;
@@ -1073,4 +1073,104 @@ impl SSubResourceData {
     pub unsafe fn raw_mut(&mut self) -> &mut D3D12_SUBRESOURCE_DATA {
         &mut self.raw
     }
+}
+
+pub enum ECompile {
+    Debug,
+    SkipValidation,
+    SkipOptimization,
+    PackMatrixRowMajor,
+    PackMatrixColumnMajor,
+    PartialPrecision,
+    ForceVsSoftwareNoOpt,
+    ForcePsSoftwareNoOpt,
+    NoPreshader,
+    AvoidFlowControl,
+    PreferFlowControl,
+    EnableStrictness,
+    EnableBackwardsCompatibility,
+    IEEEStrictness,
+    OptimizationLevel0,
+    OptimizationLevel1,
+    OptimizationLevel2,
+    OptimizationLevel3,
+    WarningsAreErrors,
+    ResourcesMayAlias,
+    //EnableUnboundedDescriptorTables,
+    AllResourcesBound,
+}
+
+impl TD3DFlags32 for ECompile {
+    type TD3DType = DWORD;
+
+    fn d3dtype(&self) -> Self::TD3DType {
+        match self {
+            ECompile::Debug => d3dcompiler::D3DCOMPILE_DEBUG,
+            ECompile::SkipValidation => d3dcompiler::D3DCOMPILE_SKIP_VALIDATION,
+            ECompile::SkipOptimization => d3dcompiler::D3DCOMPILE_SKIP_OPTIMIZATION,
+            ECompile::PackMatrixRowMajor => d3dcompiler::D3DCOMPILE_PACK_MATRIX_ROW_MAJOR,
+            ECompile::PackMatrixColumnMajor => d3dcompiler::D3DCOMPILE_PACK_MATRIX_COLUMN_MAJOR,
+            ECompile::PartialPrecision => d3dcompiler::D3DCOMPILE_PARTIAL_PRECISION,
+            ECompile::ForceVsSoftwareNoOpt => d3dcompiler::D3DCOMPILE_FORCE_VS_SOFTWARE_NO_OPT,
+            ECompile::ForcePsSoftwareNoOpt => d3dcompiler::D3DCOMPILE_FORCE_PS_SOFTWARE_NO_OPT,
+            ECompile::NoPreshader => d3dcompiler::D3DCOMPILE_NO_PRESHADER,
+            ECompile::AvoidFlowControl => d3dcompiler::D3DCOMPILE_AVOID_FLOW_CONTROL,
+            ECompile::PreferFlowControl => d3dcompiler::D3DCOMPILE_PREFER_FLOW_CONTROL,
+            ECompile::EnableStrictness => d3dcompiler::D3DCOMPILE_ENABLE_STRICTNESS,
+            ECompile::EnableBackwardsCompatibility => d3dcompiler::D3DCOMPILE_ENABLE_BACKWARDS_COMPATIBILITY,
+            ECompile::IEEEStrictness => d3dcompiler::D3DCOMPILE_IEEE_STRICTNESS,
+            ECompile::OptimizationLevel0 => d3dcompiler::D3DCOMPILE_OPTIMIZATION_LEVEL0,
+            ECompile::OptimizationLevel1 => d3dcompiler::D3DCOMPILE_OPTIMIZATION_LEVEL1,
+            ECompile::OptimizationLevel2 => d3dcompiler::D3DCOMPILE_OPTIMIZATION_LEVEL2,
+            ECompile::OptimizationLevel3 => d3dcompiler::D3DCOMPILE_OPTIMIZATION_LEVEL3,
+            ECompile::WarningsAreErrors => d3dcompiler::D3DCOMPILE_WARNINGS_ARE_ERRORS,
+            ECompile::ResourcesMayAlias => d3dcompiler::D3DCOMPILE_RESOURCES_MAY_ALIAS,
+            //ECompile::EnableUnboundedDescriptorTables => d3dcompiler::D3DCOMPILE_ENABLE_UNBOUND_DESCRIPTOR_TABLES,
+            ECompile::AllResourcesBound => d3dcompiler::D3DCOMPILE_ALL_RESOURCES_BOUND,
+        }
+    }
+}
+
+pub type SCompile = SD3DFlags32<ECompile>;
+
+pub struct SBlob {
+    raw: ComPtr<d3dcommon::ID3DBlob>,
+}
+
+// -- $$$FRK(TODO): unsupported:
+// --    + pDefines
+// --    + pInclude
+// --    + flags2
+pub fn d3dcompilefromfile(file: &str, entrypoint: &str, target: &str, flags1: SCompile) -> Result<SBlob, &'static str> {
+    // -- $$$FRK(TODO): allocations :(
+    let mut fileparam: Vec<u16> = file.encode_utf16().collect();
+    fileparam.push('\0' as u16);
+
+    let mut entrypointparam: Vec<char> = entrypoint.chars().collect();
+    entrypointparam.push('\0');
+
+    let mut targetparam: Vec<char> = target.chars().collect();
+    targetparam.push('\0');
+
+    let mut rawcodeblob: *mut d3dcommon::ID3DBlob = ptr::null_mut();
+    let mut errormsgsblob: *mut d3dcommon::ID3DBlob = ptr::null_mut();
+
+    let hr = unsafe { d3dcompiler::D3DCompileFromFile(
+        fileparam.as_ptr(),
+        ptr::null_mut(),
+        ptr::null_mut(),
+        entrypointparam.as_ptr() as *const i8,
+        targetparam.as_ptr() as *const i8,
+        flags1.d3dtype(),
+        0,
+        &mut rawcodeblob,
+        &mut errormsgsblob,
+    ) };
+
+    returnerrifwinerror!(hr, "failed to compile shader");
+    // -- $$$FRK(TODO): use error messages blob
+
+    Ok(SBlob{
+        raw: unsafe { ComPtr::from_raw(rawcodeblob) },
+    })
 }

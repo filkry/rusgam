cf523570cf843bb3ab4885db4e6a6a6b4f6d6e83
Fil Krynicki <filipkrynicki@gmail.com>
2019-12-30 14:00:48 -0800

    Simple fake directional lighting calc

--- shaders/pixel.hlsl
+++ shaders/pixel.hlsl
@@ -17,10 +17,14 @@ SamplerState g_sampler : register(s0);
 
 float4 main( SPixelShaderInput input ) : SV_Target
 {
-    //if(texture_metadata_buffer.is_textured > 0.0f)
-    //    return g_texture.Sample(g_sampler, input.uv);
-    //else
-    //    return input.color;
+    float3 light_dir = normalize(float3(-1.0, -1.0, -1.0));
+    float simple_light_weight = saturate(dot(light_dir, -input.normal.xyz));
 
-    return input.normal;
+    float4 base_colour;
+    if(texture_metadata_buffer.is_textured > 0.0f)
+        base_colour = g_texture.Sample(g_sampler, input.uv);
+    else
+        base_colour = input.color;
+
+    return base_colour * simple_light_weight;
 }

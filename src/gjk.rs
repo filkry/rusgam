66262a6c48f46f9a54ee97d2cd7a86e55944c7aa
Fil Krynicki <filipkrynicki@gmail.com>
2020-05-23 11:59:28 -0700

    I think I have an implementation of simplex vorinoi testing that makes sense to me

--- src/gjk.rs
+++ src/gjk.rs
@@ -106,54 +106,47 @@ impl S3Simplex {
 
         let ab = self.b - self.a;
         let ac = self.c - self.a;
-        let ao = -a;
+        let ao = -self.a;
 
         // -- If we are in A simplex
         if (glm::dot(a0, ab) <= 0) && (glm::dot(a0, ac) <= 0) {
             return update_simplex_result1(self.a, ao);
         }
+        // -- A eliminated, AB, AC, ABC, -ABC remain
 
         let abc_perp = glm::cross(ab, ac);
 
-        let ac_perp_on_tri_plane = glm::cross(abc_perp, ac);
-        let ab_perp_on_tri_plane = glm::cross(ab, abc_perp);
-
-        if glm::dot(ac_perp_on_tri_plane, a0) > 0  {
-            // -- excludes ABC, since this is effectively a plane side test for one plane of the
-            // -- triangles's corresponding prism
-
-            // -- I think this excludes AB, since if a point was outside the planes AB and AC, it
-            // -- would necessarily be further along the previous search direction than A
-            break_assert!(glm::dot(ab_perp_on_tri_plane, a0) <= 0);
-
-            // -- thus, the result must be AC
-            let ac_perp_to_origin = glm::cross(glm::cross(ac, ao), ac);
-
-            update_simplex_result2(self.a, self.c, ac_perp_to_origin)
+        fn check_edge(a: &Vec3, ao: &Vec3, p: &Vec3, ap: &Vec3, abc_perp: &Vec3) -> bool {
+            // -- inequalities from Real-time collision detection
+            let ineq1 = glm::dot(ao, ap) >= 0.0;
+            let ineq2 = glm::dot(-p, a - p) >= 0.0;
+            let ineq3 = glm::dot(ao, glm::cross(ap, abc_perp)) >= 0.0;
+            return ineq1 && ineq2 && ineq3;
         }
-        // -- AC excluded beyond here
-        else if glm::dot(ab_perp_on_tri_plane, a0) > 0  {
-            // -- excludes ABC, since this is effectively a plane side test for one plane of the
-            // -- triangles's corresponding prism
 
-            // -- thus, the result must be AB
+        // -- check AB
+        if(check_edge(self.a, &ao, self.b, &ab, &abc_perp)) {
             let ab_perp_to_origin = glm::cross(glm::cross(ab, ao), ab);
-            update_simplex_result2(self.a, self.b, ab_perp_to_origin)
+            return update_simplex_result2(self.a, self.b, ab_perp_to_origin);
         }
-        // -- AB, AC excluded beyond here
-        else {
-            // -- need to determine if we are above ABC or below
-            if glm::dot(abc_perp, ao) > 0 {
-                // -- above
-                update_simplex_result3(self.a, self.b, self.c, abc_perp);
-            }
-            else {
-                // -- below
-                // -- need to swizzle results, as we'll rely on the order to determine "outside"
-                // -- face direction in Simplex4 case
 
-                update_simplex_result3(self.a, self.c, self.b, -abc_perp);
-            }
+        // -- check AC
+        if(check_edge(self.a, &ao, self.c, &ac, &abc_perp)) {
+            let ac_perp_to_origin = glm::cross(glm::cross(ac, ao), ac);
+            return update_simplex_result2(self.a, self.c, ac_perp_to_origin);
+        }
+        // -- AB, AC eliminated, ABC, -ABC remain
+
+        // -- need to determine if we are above ABC or below
+        if glm::dot(abc_perp, ao) > 0 {
+            // -- above
+            update_simplex_result3(self.a, self.b, self.c, abc_perp);
+        }
+        else {
+            // -- below
+            // -- need to swizzle results, as we'll rely on the order to determine "outside"
+            // -- face direction in Simplex4 case
+            update_simplex_result3(self.a, self.c, self.b, -abc_perp);
         }
     }
 }
@@ -161,16 +154,12 @@ impl S3Simplex {
 impl S4Simplex {
     pub fn update_simplex(&self) -> (ESimplex, Vec3) {
         // -- possible voronoi regions:
-        // -- A, B, C, D, AB, AC, AD, BC, BD, CD, ABC, ABD, ACD, BCD
-        // -- A is always the furthest point in the direction perp to ABC, in the direction of O
-        // -- Again, we can exclude any not including A, and A itself, leaving:
-        // -- AB, AC, AD, ABC, ABD, ACD
+        // -- A, B, C, D, AB, AC, AD, BC, BD, CD, ABC, ABD, ACD, BCD (no negative triangles because if it's inside we are done)
+        // -- by reasoning above, only features including A remain:
+        // -- A, AB, AC, AD, ABC, ABD, ACD
 
         // -- verifying triangle winding
         break_assert!(glm::dot(self.a - self.b, glm::cross(self.c - self.b, self.d - self.b)) > 0);
-
-        // -- we will take an easier approach, and try and figure out which triangle (if any) is
-        // -- closest to origin, then fall back to S3Simplex::update_simplex
 
         let ab = self.b - self.a;
         let ac = self.c - self.a;
@@ -181,27 +170,66 @@ impl S4Simplex {
         let abd_perp = glm::cross(ad, ab);
         let acd_perp = glm::cross(ac, ad);
 
-        if glm::dot(abc_perp, a0) > 0 {
-            // -- could be closest to ABC, ABD or ACD, but not inside
+        // -- check containment
 
+        if glm::dot(abc_perp, a0) <= 0.0 && glm::dot(abd_perp, a0) <= 0.0 && glm::dot(acd_perp, ao) <= 0.0 {
+            return COLLISION;
         }
-        else if glm::dot(abd_perp, a0) > 0 {
-            // -- could be closest to ABD or ACD
 
+        // -- If we are in A simplex
+        if (glm::dot(a0, ab) <= 0) && (glm::dot(a0, ac) <= 0) && (glm::dot(a0, ad) <= 0) {
+            return update_simplex_result1(self.a, ao);
         }
-        else if glm::dot(acd_perp, a0) > 0 {
-            // -- must be closest to ACD
-            let acd_simplex = S3Simplex{
-                a: self.a,
-                b: self.c,
-                c: self.d,
-            };
-            return acd_simplex.update_simplex();
+        // -- A eliminated, AB, AC, AD, ABC, ABD, ACD remain
+
+        // -- here, AP must be counter-clockwise on the counter_clockwise_triangle, and clockwise
+        // -- on the clockwise_triangle
+        fn check_edge(a: &Vec3, ao: &Vec3, p: &Vec3, ap: &Vec3, counter_clockwise_triangle_perp: &Vec3, clockwise_triangle_perp: &Vec3) -> bool {
+            // -- inequalities from Real-time collision detection
+            let ineq1 = glm::dot(ao, ap) >= 0.0;
+            let ineq2 = glm::dot(-p, a - p) >= 0.0;
+            let ineq3 = glm::dot(ao, glm::cross(ap, counter_clockwise_triangle_perp)) >= 0.0;
+            let ineq4 = glm::dot(ao, glm::cross(clockwise_triangle_perp, ap)) >= 0.0;
+            return ineq1 && ineq2 && ineq3;
         }
-        else {
-            // -- intersection, we are not outside any of the planes
-            let bcd_perp = glm::cross(self.c - self.b, self.d - self.b);
-            break_assert!(glm::dot(bcd_perp, a0) <= 0);
+
+        // -- check AB
+        if check_edge(self.a, &ao, self.b, &ab, &abc_perp, &abd_perp) {
+            let ab_perp_to_origin = glm::cross(glm::cross(ab, ao), ab);
+            return update_simplex_result2(self.a, self.b, ab_perp_to_origin);
+        }
+
+        // -- check AC
+        if check_edge(self.a, &ao, self.c, &ac, &acd_perp, &abc_perp) {
+            let ac_perp_to_origin = glm::cross(glm::cross(ac, ao), ac);
+            return update_simplex_result2(self.a, self.c, ac_perp_to_origin);
+        }
+
+        // -- check AD
+        if check_edge(self.a, &ao, self.d, &ad, &abd_perp, &acd_perp) {
+            let ad_perp_to_origin = glm::cross(glm::cross(ad, ao), ad);
+            return update_simplex_result2(self.a, self.d, ad_perp_to_origin);
+        }
+
+        // -- AB, AC, AD elimineated, ABC, ABD, ACD remain
+
+        fn check_face(a: &Vec3, ao: &Vec3, non_face_p: &Vec3, face_perp: &Vec3) -> bool {
+            return (glm::dot(a0, face_perp) * glm::dot(non_face_p - a, face_perp)) < 0.0;
+        }
+
+        // -- check ABC
+        if check_face(self.a, &ao, self.d, &abc_perp) {
+            return update_simplex_result3(self.a, self.b, self.c, abc_perp);
+        }
+
+        // -- check ABD
+        if check_face(self.a, &ao, self.c, &abd_perp) {
+            return update_simplex_result3(self.a, self.b, self.d, abd_perp);
+        }
+
+        // -- check ACD
+        if check_face(self.a, &ao, self.b, &acd_perp) {
+            return update_simplex_result3(self.a, self.c, self.d, acd_perp);
         }
     }
 }
